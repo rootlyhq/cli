3215dda10893f38439a6be73f6469058940d9100
Matthew Gleich <git@mattglei.ch>
2021-02-18 22:42:30 -0500

    ✨ Create basic pulses
    Signed-off-by: Matthew Gleich <git@mattglei.ch>

--- cmd/rootly/main.go
+++ cmd/rootly/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/deepmap/oapi-codegen/pkg/securityprovider"
+	"github.com/rootly-io/rootly.go"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+
+	bearerToken, err := securityprovider.NewSecurityProviderBearerToken("AUTH TOKEN")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	client, err := rootly.NewClient("https://api.rootly.io")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	req, err := rootly.NewCreatePulseRequestWithBody("https://api.rootly.io", "application/vnd.api+json", strings.NewReader("{\"data\": {\"type\": \"pulses\",\"attributes\": {\"summary\": \"hello world\"}}}"))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = bearerToken.Intercept(req.Context(), req)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	_, err = client.Client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/rootly-io/cli
 
 go 1.15
+
+require (
+	github.com/deepmap/oapi-codegen v1.5.1
+	github.com/go-openapi/swag v0.19.14 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rootly-io/rootly.go v0.0.0-20210218215936-e087955620e8
+	golang.org/x/crypto v0.0.0-20210218145215-b8e89b74b9df // indirect
+	golang.org/x/sys v0.0.0-20210218155724-8ebf48af031b // indirect
+)
