17beded3df285c441f018be17a7e972ae65637f6
Matthew Gleich <git@mattglei.ch>
2021-02-20 18:12:53 -0500

    📝 Add = in pulse-run example
    Signed-off-by: Matthew Gleich <git@mattglei.ch>

--- pkg/commands/pulserun/run_test.go
+++ pkg/commands/pulserun/run_test.go
@@ -1,47 +1,51 @@
 package pulserun
 
 import (
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRunProgram(t *testing.T) {
-	tt := []struct {
-		program          string
-		args             []string
-		expectingErr     bool
-		expectedExitCode int
-	}{
-		{
-			program:          "echo",
-			args:             []string{"hello", "world"},
-			expectingErr:     false,
-			expectedExitCode: 0,
-		},
-		{
-			program:          "type",
-			args:             []string{"hello", "world"},
-			expectingErr:     false,
-			expectedExitCode: 1,
-		},
-		{
-			program:          "program_that_doesnt_exist",
-			args:             []string{"hello", "world"},
-			expectingErr:     true,
-			expectedExitCode: 0,
-		},
-	}
-
-	for _, test := range tt {
-		exitCode, err := RunProgram(test.program, test.args)
-
-		if test.expectingErr {
-			assert.Error(t, err.Error)
-		} else {
-			assert.NoError(t, err.Error)
+	// Only run unit tests in linux or macOS
+	if runtime.GOOS == "linux" || runtime.GOOS == "darwin" {
+		tt := []struct {
+			program          string
+			args             []string
+			expectingErr     bool
+			expectedExitCode int
+		}{
+			{
+				program:          "echo",
+				args:             []string{"hello", "world"},
+				expectingErr:     false,
+				expectedExitCode: 0,
+			},
+			{
+				program:          "cat",
+				args:             []string{"hello", "world"},
+				expectingErr:     false,
+				expectedExitCode: 1,
+			},
+			{
+				program:          "program_that_doesnt_exist",
+				args:             []string{"hello", "world"},
+				expectingErr:     true,
+				expectedExitCode: 0,
+			},
 		}
 
-		assert.Equal(t, test.expectedExitCode, exitCode)
+		for _, test := range tt {
+			exitCode, err := RunProgram(test.program, test.args)
+
+			if test.expectingErr {
+				assert.Error(t, err.Error)
+			} else {
+				assert.NoError(t, err.Error)
+			}
+
+			assert.Equal(t, test.expectedExitCode, exitCode)
+		}
 	}
 }
