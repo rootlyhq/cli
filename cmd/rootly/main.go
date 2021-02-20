bdd16dee02994af1e45147c251a2a214a82c2705
Matthew Gleich <git@mattglei.ch>
2021-02-20 15:08:02 -0500

    🔥 Remove old comments from main
    Signed-off-by: Matthew Gleich <git@mattglei.ch>

--- cmd/rootly/main.go
+++ cmd/rootly/main.go
@@ -6,25 +6,6 @@ import (
 )
 
 func main() {
-
 	lumber.ErrNilCheck = false
 	commands.Execute()
-
-	// req, err := rootly.NewCreatePulseRequestWithBody("https://api.rootly.io", "application/vnd.api+json", strings.NewReader("{\"data\": {\"type\": \"pulses\",\"attributes\": {\"summary\": \"hello world\"}}}"))
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(1)
-	// }
-
-	// err = bearerToken.Intercept(req.Context(), req)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(1)
-	// }
-
-	// _, err = client.Client.Do(req)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(1)
-	// }
 }
