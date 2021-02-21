d5f6e5feed3293f3260554e4dea16eb48ba7f37d
Matthew Gleich <git@mattglei.ch>
2021-02-20 23:10:42 -0500

    ♻️ Refactor to use rootly.NewPulse
    Signed-off-by: Matthew Gleich <git@mattglei.ch>

--- pkg/api/convert.go
+++ pkg/api/convert.go
@@ -2,19 +2,18 @@ package api
 
 import (
 	"encoding/json"
-	"time"
 
 	"github.com/rootly-io/cli/pkg/log"
 	"github.com/rootly-io/rootly-go"
 )
 
-// Convert a Pulse to a rootly.Pulse
+// Convert a Pulse to a json version of rootly.NewPulse
 func convertPulse(pulse Pulse) (string, log.CtxErr) {
-	// Converting Pulse.Labels to rootly.Pulse.Lables
-	var labels []struct {
+	// Converting Pulse.Labels to rootly.NewPulse.Lables
+	labels := []struct {
 		Key   string `json:"key"`
 		Value string `json:"value"`
-	}
+	}{}
 	for _, label := range pulse.Labels {
 		labels = append(labels, struct {
 			Key   string `json:"key"`
@@ -25,28 +24,30 @@ func convertPulse(pulse Pulse) (string, log.CtxErr) {
 		})
 	}
 
-	// Putting rootly.Pulse inside apiData
-	var finalData apiData
-	finalData.Data.Type = "pulses"
-
-	// We must conditionally add labels because the rootly library uses pointers
-	if len(labels) == 0 {
-		finalData.Data.Attributes = rootly.Pulse{
-			Summary:   &pulse.Summary,
-			CreatedAt: convertTime(pulse.CreatedAt),
-			UpdatedAt: convertTime(pulse.UpdatedAt),
-		}
-	} else {
-		finalData.Data.Attributes = rootly.Pulse{
-			Summary:   &pulse.Summary,
-			CreatedAt: convertTime(pulse.CreatedAt),
-			UpdatedAt: convertTime(pulse.UpdatedAt),
-			Labels:    &labels,
-		}
+	// Putting data into rootly.NewPulse
+	// We need to add optional data conditionally because the
+	// rootly library uses pointers for everything
+	var data rootly.NewPulse
+	data.Data.Type = "pulses"
+	data.Data.Attributes.Summary = &pulse.Summary
+	if !pulse.EndedAt.IsZero() {
+		data.Data.Attributes.EndedAt = &pulse.EndedAt
+	}
+	if !pulse.StartedAt.IsZero() {
+		data.Data.Attributes.StartedAt = &pulse.StartedAt
+	}
+	if len(pulse.ServiceIds) != 0 {
+		data.Data.Attributes.ServiceIds = &pulse.ServiceIds
+	}
+	if len(pulse.EnvironmentIds) != 0 {
+		data.Data.Attributes.EnvironmentIds = &pulse.EnvironmentIds
+	}
+	if len(labels) != 0 {
+		data.Data.Attributes.Labels = &labels
 	}
 
 	// Marshaling the data
-	jsonData, err := jsonMarshal(finalData)
+	jsonData, err := jsonMarshal(data)
 	if err.Error != nil {
 		return "", err
 	}
@@ -64,17 +65,4 @@ func jsonMarshal(data interface{}) (string, log.CtxErr) {
 		}
 	}
 	return string(str), log.CtxErr{}
-}
-
-// Convert time if given a value
-func convertTime(t time.Time) string {
-	var (
-		epoch     = time.Time{}.Format(time.RFC3339)
-		converted = t.Format(time.RFC3339)
-	)
-
-	if epoch == converted {
-		return ""
-	}
-	return converted
 }
--- pkg/api/convert_test.go
+++ pkg/api/convert_test.go
@@ -15,7 +15,7 @@ func TestConvertPulse(t *testing.T) {
 	}{
 		{
 			pulse:    Pulse{Summary: "Hello World!"},
-			expected: "{\"data\":{\"type\":\"pulses\",\"attributes\":{\"created_at\":\"\",\"summary\":\"Hello World!\",\"updated_at\":\"\"}}}",
+			expected: "{\"data\":{\"attributes\":{\"ended_at\":null,\"environment_ids\":null,\"service_ids\":null,\"started_at\":null,\"summary\":\"Hello World!\"},\"type\":\"pulses\"}}",
 		},
 		{
 			pulse: Pulse{
@@ -27,7 +27,7 @@ func TestConvertPulse(t *testing.T) {
 					},
 				},
 			},
-			expected: "{\"data\":{\"type\":\"pulses\",\"attributes\":{\"created_at\":\"\",\"labels\":[{\"key\":\"Name\",\"value\":\"Harry Potter\"}],\"summary\":\"Hello World!\",\"updated_at\":\"\"}}}",
+			expected: "{\"data\":{\"attributes\":{\"ended_at\":null,\"environment_ids\":null,\"labels\":[{\"key\":\"Name\",\"value\":\"Harry Potter\"}],\"service_ids\":null,\"started_at\":null,\"summary\":\"Hello World!\"},\"type\":\"pulses\"}}",
 		},
 		{
 			pulse: Pulse{
@@ -38,9 +38,9 @@ func TestConvertPulse(t *testing.T) {
 						"value": "Harry Potter",
 					},
 				},
-				UpdatedAt: time.Date(2001, 1, 1, 1, 1, 1, 1, time.UTC),
+				StartedAt: time.Date(2001, 1, 1, 1, 1, 1, 1, time.UTC),
 			},
-			expected: "{\"data\":{\"type\":\"pulses\",\"attributes\":{\"created_at\":\"\",\"labels\":[{\"key\":\"Name\",\"value\":\"Harry Potter\"}],\"summary\":\"Hello World!\",\"updated_at\":\"2001-01-01T01:01:01Z\"}}}",
+			expected: "{\"data\":{\"attributes\":{\"ended_at\":null,\"environment_ids\":null,\"labels\":[{\"key\":\"Name\",\"value\":\"Harry Potter\"}],\"service_ids\":null,\"started_at\":\"2001-01-01T01:01:01.000000001Z\",\"summary\":\"Hello World!\"},\"type\":\"pulses\"}}",
 		},
 		{
 			pulse: Pulse{
@@ -51,9 +51,10 @@ func TestConvertPulse(t *testing.T) {
 						"value": "Harry Potter",
 					},
 				},
-				CreatedAt: time.Date(2001, 1, 1, 1, 1, 1, 1, time.UTC),
+				EndedAt:   time.Date(2001, 1, 1, 1, 1, 1, 1, time.UTC),
+				StartedAt: time.Date(2001, 1, 1, 1, 1, 1, 1, time.UTC),
 			},
-			expected: "{\"data\":{\"type\":\"pulses\",\"attributes\":{\"created_at\":\"2001-01-01T01:01:01Z\",\"labels\":[{\"key\":\"Name\",\"value\":\"Harry Potter\"}],\"summary\":\"Hello World!\",\"updated_at\":\"\"}}}",
+			expected: "{\"data\":{\"attributes\":{\"ended_at\":\"2001-01-01T01:01:01.000000001Z\",\"environment_ids\":null,\"labels\":[{\"key\":\"Name\",\"value\":\"Harry Potter\"}],\"service_ids\":null,\"started_at\":\"2001-01-01T01:01:01.000000001Z\",\"summary\":\"Hello World!\"},\"type\":\"pulses\"}}",
 		},
 		{
 			pulse: Pulse{
@@ -68,8 +69,12 @@ func TestConvertPulse(t *testing.T) {
 						"value": "1",
 					},
 				},
+				EndedAt:        time.Date(2001, 1, 1, 1, 1, 1, 1, time.UTC),
+				StartedAt:      time.Date(2001, 1, 1, 1, 1, 1, 1, time.UTC),
+				ServiceIds:     []string{"elasticsearch-prod"},
+				EnvironmentIds: []string{"production", "staging"},
 			},
-			expected: "{\"data\":{\"type\":\"pulses\",\"attributes\":{\"created_at\":\"\",\"labels\":[{\"key\":\"Name\",\"value\":\"Harry Potter\"},{\"key\":\"Exit Code\",\"value\":\"1\"}],\"summary\":\"Hello World!\",\"updated_at\":\"\"}}}",
+			expected: "{\"data\":{\"attributes\":{\"ended_at\":\"2001-01-01T01:01:01.000000001Z\",\"environment_ids\":[\"production\",\"staging\"],\"labels\":[{\"key\":\"Name\",\"value\":\"Harry Potter\"},{\"key\":\"Exit Code\",\"value\":\"1\"}],\"service_ids\":[\"elasticsearch-prod\"],\"started_at\":\"2001-01-01T01:01:01.000000001Z\",\"summary\":\"Hello World!\"},\"type\":\"pulses\"}}",
 		},
 	}
 
