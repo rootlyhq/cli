dbde1f9b3b44869be51f0876c8e696c97031b110
Matthew Gleich <git@mattglei.ch>
2021-02-21 18:05:25 -0500

    🥅 Provide better CreatePule err handling
    Signed-off-by: Matthew Gleich <git@mattglei.ch>

--- pkg/api/request.go
+++ pkg/api/request.go
@@ -1,6 +1,9 @@
 package api
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strings"
 
@@ -56,8 +59,65 @@ func CreatePulse(
 		}
 	}
 
-	if resp.StatusCode != http.StatusCreated {
-		return log.NewErr("Failed to create pulse with exit code " + resp.Status + "\n\n" + data)
+	// Handeling a failed request
+	if resp.StatusCode != http.StatusOK {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return log.CtxErr{
+				Context: "Failed to read response from failed request",
+				Error:   err,
+			}
+		}
+
+		if resp.StatusCode == http.StatusCreated {
+			// Getting json data to see if serviceids or environmentids failed
+			possibleErr := "The given environments or services don't exist in rootly."
+
+			var jsonResp rootly.NewPulse
+			err = json.Unmarshal(body, &jsonResp)
+			if err != nil {
+				return log.CtxErr{
+					Context: "Failed to parse json from failed request response. " + possibleErr,
+					Error:   err,
+				}
+			}
+
+			// If there is an invalid service id
+			if jsonResp.Data.Attributes.ServiceIds == nil && len(pulse.ServiceIds) != 0 {
+				if len(pulse.ServiceIds) == 1 {
+					return log.NewErr(pulse.ServiceIds[0] + " is not a valid service.")
+				}
+				return log.NewErr(
+					"Please check your list of services. One is not valid: " + strings.Join(
+						pulse.ServiceIds,
+						", ",
+					),
+				)
+			}
+
+			// If there is an invalid environment id
+			if jsonResp.Data.Attributes.EnvironmentIds == nil &&
+				len(pulse.EnvironmentIds) != 0 {
+				if len(pulse.EnvironmentIds) == 1 {
+					return log.NewErr(pulse.EnvironmentIds[0] + " is not a valid environment.")
+				}
+				return log.NewErr(
+					"Please check your list of environments. One is not valid: " + strings.Join(
+						pulse.EnvironmentIds,
+						", ",
+					),
+				)
+			}
+		}
+
+		return log.NewErr(
+			fmt.Sprintf(
+				"Failed to create pulse with exit code %v\n\nPayload:\n%v\n\nResponse:\n%v",
+				resp.Status,
+				data,
+				string(body),
+			),
+		)
 	}
 
 	log.Success("Created a pulse with the following values:", fmtPulse)
