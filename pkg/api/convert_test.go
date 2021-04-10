0e651f034768d528ceb66c9686a53ccd0ca73970
Matthew Gleich <git@mattglei.ch>
2021-04-10 13:43:28 -0400

    test: update tests to match
    Signed-off-by: Matthew Gleich <git@mattglei.ch>

--- pkg/api/convert_test.go
+++ pkg/api/convert_test.go
@@ -16,7 +16,7 @@ func TestConvertPulse(t *testing.T) {
 	}{
 		{
 			pulse:    models.Pulse{Summary: "Hello World!"},
-			expected: "{\"data\":{\"attributes\":{\"ended_at\":null,\"environment_ids\":null,\"service_ids\":null,\"started_at\":null,\"summary\":\"Hello World!\"},\"type\":\"pulses\"}}",
+			expected: "{\"data\":{\"attributes\":{\"ended_at\":null,\"environment_ids\":null,\"service_ids\":null,\"source\":\"\",\"started_at\":null,\"summary\":\"Hello World!\"},\"type\":\"pulses\"}}",
 		},
 		{
 			pulse: models.Pulse{
@@ -28,7 +28,7 @@ func TestConvertPulse(t *testing.T) {
 					},
 				},
 			},
-			expected: "{\"data\":{\"attributes\":{\"ended_at\":null,\"environment_ids\":null,\"labels\":[{\"key\":\"platform\",\"value\":\"osx\"}],\"service_ids\":null,\"started_at\":null,\"summary\":\"Hello World!\"},\"type\":\"pulses\"}}",
+			expected: "{\"data\":{\"attributes\":{\"ended_at\":null,\"environment_ids\":null,\"labels\":[{\"key\":\"platform\",\"value\":\"osx\"}],\"service_ids\":null,\"source\":\"\",\"started_at\":null,\"summary\":\"Hello World!\"},\"type\":\"pulses\"}}",
 		},
 		{
 			pulse: models.Pulse{
@@ -41,7 +41,7 @@ func TestConvertPulse(t *testing.T) {
 				},
 				StartedAt: time.Date(2001, 1, 1, 1, 1, 1, 1, time.UTC),
 			},
-			expected: "{\"data\":{\"attributes\":{\"ended_at\":null,\"environment_ids\":null,\"labels\":[{\"key\":\"platform\",\"value\":\"osx\"}],\"service_ids\":null,\"started_at\":\"2001-01-01T01:01:01.000000001Z\",\"summary\":\"Hello World!\"},\"type\":\"pulses\"}}",
+			expected: "{\"data\":{\"attributes\":{\"ended_at\":null,\"environment_ids\":null,\"labels\":[{\"key\":\"platform\",\"value\":\"osx\"}],\"service_ids\":null,\"source\":\"\",\"started_at\":\"2001-01-01T01:01:01.000000001Z\",\"summary\":\"Hello World!\"},\"type\":\"pulses\"}}",
 		},
 		{
 			pulse: models.Pulse{
@@ -55,7 +55,7 @@ func TestConvertPulse(t *testing.T) {
 				EndedAt:   time.Date(2001, 1, 1, 1, 1, 1, 1, time.UTC),
 				StartedAt: time.Date(2001, 1, 1, 1, 1, 1, 1, time.UTC),
 			},
-			expected: "{\"data\":{\"attributes\":{\"ended_at\":\"2001-01-01T01:01:01.000000001Z\",\"environment_ids\":null,\"labels\":[{\"key\":\"platform\",\"value\":\"osx\"}],\"service_ids\":null,\"started_at\":\"2001-01-01T01:01:01.000000001Z\",\"summary\":\"Hello World!\"},\"type\":\"pulses\"}}",
+			expected: "{\"data\":{\"attributes\":{\"ended_at\":\"2001-01-01T01:01:01.000000001Z\",\"environment_ids\":null,\"labels\":[{\"key\":\"platform\",\"value\":\"osx\"}],\"service_ids\":null,\"source\":\"\",\"started_at\":\"2001-01-01T01:01:01.000000001Z\",\"summary\":\"Hello World!\"},\"type\":\"pulses\"}}",
 		},
 		{
 			pulse: models.Pulse{
@@ -74,8 +74,19 @@ func TestConvertPulse(t *testing.T) {
 				StartedAt:      time.Date(2001, 1, 1, 1, 1, 1, 1, time.UTC),
 				ServiceIds:     []string{"elasticsearch-prod"},
 				EnvironmentIds: []string{"production", "staging"},
+				Source:         "k8s",
+				Refs: []map[string]string{
+					{
+						"key":   "sha",
+						"value": "cd62148cbc5eb42168fe99fdb50a364e12b206ac",
+					},
+					{
+						"key":   "image",
+						"value": "registry.rootly.io/rootly/my-service:cd6214",
+					},
+				},
 			},
-			expected: "{\"data\":{\"attributes\":{\"ended_at\":\"2001-01-01T01:01:01.000000001Z\",\"environment_ids\":[\"production\",\"staging\"],\"labels\":[{\"key\":\"platform\",\"value\":\"osx\"},{\"key\":\"Exit Code\",\"value\":\"1\"}],\"service_ids\":[\"elasticsearch-prod\"],\"started_at\":\"2001-01-01T01:01:01.000000001Z\",\"summary\":\"Hello World!\"},\"type\":\"pulses\"}}",
+			expected: "{\"data\":{\"attributes\":{\"ended_at\":\"2001-01-01T01:01:01.000000001Z\",\"environment_ids\":[\"production\",\"staging\"],\"labels\":[{\"key\":\"platform\",\"value\":\"osx\"},{\"key\":\"Exit Code\",\"value\":\"1\"}],\"refs\":[{\"key\":\"sha\",\"value\":\"cd62148cbc5eb42168fe99fdb50a364e12b206ac\"},{\"key\":\"image\",\"value\":\"registry.rootly.io/rootly/my-service:cd6214\"}],\"service_ids\":[\"elasticsearch-prod\"],\"source\":\"k8s\",\"started_at\":\"2001-01-01T01:01:01.000000001Z\",\"summary\":\"Hello World!\"},\"type\":\"pulses\"}}",
 		},
 	}
 
--- pkg/log/format_test.go
+++ pkg/log/format_test.go
@@ -17,43 +17,43 @@ func TestFormatPulse(t *testing.T) {
 			pulse: models.Pulse{
 				Summary: "Basic Pulse",
 			},
-			expected: "\n\n  Summary:      Basic Pulse\n  Started At:   01 Jan 01 00:00 UTC\n  Ended At      01 Jan 01 00:00 UTC\n  Services:     None\n  Environments: None\n  Labels:       None",
+			expected: "\n\n  Summary:      Basic Pulse\n  Started At:   01 Jan 01 00:00 UTC\n  Ended At      01 Jan 01 00:00 UTC\n  Services:     None\n  Environments: None\n  Labels:       None\n  Source:       None\n  Refs:         None",
 		},
 		{
 			pulse: models.Pulse{
 				StartedAt: time.Date(2001, 1, 1, 1, 1, 1, 1, time.UTC),
 			},
-			expected: "\n\n  Summary:      None\n  Started At:   01 Jan 01 01:01 UTC\n  Ended At      01 Jan 01 00:00 UTC\n  Services:     None\n  Environments: None\n  Labels:       None",
+			expected: "\n\n  Summary:      None\n  Started At:   01 Jan 01 01:01 UTC\n  Ended At      01 Jan 01 00:00 UTC\n  Services:     None\n  Environments: None\n  Labels:       None\n  Source:       None\n  Refs:         None",
 		},
 		{
 			pulse: models.Pulse{
 				EndedAt: time.Date(2001, 1, 1, 1, 1, 1, 1, time.UTC),
 			},
-			expected: "\n\n  Summary:      None\n  Started At:   01 Jan 01 00:00 UTC\n  Ended At      01 Jan 01 01:01 UTC\n  Services:     None\n  Environments: None\n  Labels:       None",
+			expected: "\n\n  Summary:      None\n  Started At:   01 Jan 01 00:00 UTC\n  Ended At      01 Jan 01 01:01 UTC\n  Services:     None\n  Environments: None\n  Labels:       None\n  Source:       None\n  Refs:         None",
 		},
 		{
 			pulse: models.Pulse{
 				ServiceIds: []string{"Service 1"},
 			},
-			expected: "\n\n  Summary:      None\n  Started At:   01 Jan 01 00:00 UTC\n  Ended At      01 Jan 01 00:00 UTC\n  Services:     Service 1\n  Environments: None\n  Labels:       None",
+			expected: "\n\n  Summary:      None\n  Started At:   01 Jan 01 00:00 UTC\n  Ended At      01 Jan 01 00:00 UTC\n  Services:     Service 1\n  Environments: None\n  Labels:       None\n  Source:       None\n  Refs:         None",
 		},
 		{
 			pulse: models.Pulse{
 				ServiceIds: []string{"Service 1", "Service 2"},
 			},
-			expected: "\n\n  Summary:      None\n  Started At:   01 Jan 01 00:00 UTC\n  Ended At      01 Jan 01 00:00 UTC\n  Services:     Service 1, Service 2\n  Environments: None\n  Labels:       None",
+			expected: "\n\n  Summary:      None\n  Started At:   01 Jan 01 00:00 UTC\n  Ended At      01 Jan 01 00:00 UTC\n  Services:     Service 1, Service 2\n  Environments: None\n  Labels:       None\n  Source:       None\n  Refs:         None",
 		},
 		{
 			pulse: models.Pulse{
 				EnvironmentIds: []string{"Environment 1", "Service 2"},
 			},
-			expected: "\n\n  Summary:      None\n  Started At:   01 Jan 01 00:00 UTC\n  Ended At      01 Jan 01 00:00 UTC\n  Services:     None\n  Environments: Environment 1, Service 2\n  Labels:       None",
+			expected: "\n\n  Summary:      None\n  Started At:   01 Jan 01 00:00 UTC\n  Ended At      01 Jan 01 00:00 UTC\n  Services:     None\n  Environments: Environment 1, Service 2\n  Labels:       None\n  Source:       None\n  Refs:         None",
 		},
 		{
 			pulse: models.Pulse{
 				EnvironmentIds: []string{"Environment 1", "Environment 2"},
 			},
-			expected: "\n\n  Summary:      None\n  Started At:   01 Jan 01 00:00 UTC\n  Ended At      01 Jan 01 00:00 UTC\n  Services:     None\n  Environments: Environment 1, Environment 2\n  Labels:       None",
+			expected: "\n\n  Summary:      None\n  Started At:   01 Jan 01 00:00 UTC\n  Ended At      01 Jan 01 00:00 UTC\n  Services:     None\n  Environments: Environment 1, Environment 2\n  Labels:       None\n  Source:       None\n  Refs:         None",
 		},
 		{
 			pulse: models.Pulse{
@@ -61,7 +61,7 @@ func TestFormatPulse(t *testing.T) {
 					{"key": "Exit Code", "value": "0"},
 				},
 			},
-			expected: "\n\n  Summary:      None\n  Started At:   01 Jan 01 00:00 UTC\n  Ended At      01 Jan 01 00:00 UTC\n  Services:     None\n  Environments: None\n  Labels:       \n    Exit Code = 0",
+			expected: "\n\n  Summary:      None\n  Started At:   01 Jan 01 00:00 UTC\n  Ended At      01 Jan 01 00:00 UTC\n  Services:     None\n  Environments: None\n  Labels:       \n    Exit Code = 0\n\n  Source:       None\n  Refs:         None",
 		},
 		{
 			pulse: models.Pulse{
@@ -69,12 +69,16 @@ func TestFormatPulse(t *testing.T) {
 					{"key": "Exit Code", "value": "0"},
 					{"key": "Version", "value": "1.0.0"},
 				},
+				Refs: []map[string]string{
+					{"key": "sha", "value": "cd62148cbc5eb42168fe99fdb50a364e12b206ac"},
+					{"key": "image", "value": "registry.rootly.io/rootly/my-service:cd6214"},
+				},
 			},
-			expected: "\n\n  Summary:      None\n  Started At:   01 Jan 01 00:00 UTC\n  Ended At      01 Jan 01 00:00 UTC\n  Services:     None\n  Environments: None\n  Labels:       \n    Exit Code = 0\n    Version = 1.0.0",
+			expected: "\n\n  Summary:      None\n  Started At:   01 Jan 01 00:00 UTC\n  Ended At      01 Jan 01 00:00 UTC\n  Services:     None\n  Environments: None\n  Labels:       \n    Exit Code = 0\n    Version = 1.0.0\n\n  Source:       None\n  Refs:         \n    sha = cd62148cbc5eb42168fe99fdb50a364e12b206ac\n    image = registry.rootly.io/rootly/my-service:cd6214",
 		},
 		{
 			pulse:    models.Pulse{},
-			expected: "\n\n  Summary:      None\n  Started At:   01 Jan 01 00:00 UTC\n  Ended At      01 Jan 01 00:00 UTC\n  Services:     None\n  Environments: None\n  Labels:       None",
+			expected: "\n\n  Summary:      None\n  Started At:   01 Jan 01 00:00 UTC\n  Ended At      01 Jan 01 00:00 UTC\n  Services:     None\n  Environments: None\n  Labels:       None\n  Source:       None\n  Refs:         None",
 		},
 	}
 
