dfa86e128a87a6355ff35babb553db6f6099940f
Quentin Rousseau <contact@quent.in>
2025-12-04 11:23:30 -0800

    Update version to v1.3.1

--- pkg/commands/root.go
+++ pkg/commands/root.go
@@ -31,7 +31,7 @@ var rootCmd = &cobra.Command{
 		}
 
 		if versionFlag {
-			version := "v1.3.0"
+			version := "v1.3.1"
 			outdated, v, err := release.Check(version, "https://github.com/rootlyhq/cli")
 
 			if err != nil {
